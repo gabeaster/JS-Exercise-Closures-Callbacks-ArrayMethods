7706edb8680d5f5e76346e82190a3db94ac44a0e
gabeaster <gab.easter@live.com>
2020-02-25 18:38:21 -0500

    mvp met ON TIME!

--- index.js
+++ index.js
@@ -270,11 +270,12 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
+  let count = 0;
   function counter() {
-    ++count
+    return count++
   }
   // BROKEN CODE ENDS
+  return counter;
 }
 
 /**
@@ -297,8 +298,14 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(limit) {
+  let count = -1;
+  return function counter(){
+    if (count === limit){
+      count = -1
+    }
+    return ++count;
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
